964e183a6881a0190a526915f393b9559ea713c8
Janelle <janelledaguiso@gmail.com>
2025-07-04 15:05:05 +0800

    Task#4_new

---
+++ main2.c
@@ -0,0 +1,55 @@
+/***********************************************************************************
+ * This file is the driver program to compute for the convex hull of a set of 
+ * coordinates using Graham's Scan algorithm (fast sorting algorithm). It reads 
+ * entries from a text file, computes for the convex hull, and outputs a text
+ * file containing the coordinates of the convex hull.
+************************************************************************************/
+#include <stdio.h>
+#include "graham_scan2.c"
+#include "graham_scan2.h"
+
+
+// BTWs
+// this file is for getting input from a file, running graham's scan, and printing the output in a file
+// not for implementing graham's scan,, there's a file for that
+
+
+int main() {
+    FILE *fp;
+    String infilename;
+    String outfilename;
+    int i=0, j;
+    int n;                          // input / problem size
+    struct Point points[MAX];
+    struct Stack S;
+    create(&S);
+
+    printf("Enter the input filename and extension: ");
+    scanf("%s", infilename);
+
+    printf("Enter the output filename and extension: ");
+    scanf("%s", outfilename);
+
+    // read input file
+    fp = fopen(infilename, "r");
+    if ((fp = fopen(infilename, "r")) == NULL)
+        fprintf(stderr, "Error %s does not exist.\n", infilename);
+
+    // read the first line -> input size
+    fscanf(fp, "%d", &n);
+    while(fscanf(fp, "%lf    %lf\n", &points[i].x, &points[i].y) == 2)
+        i++;
+    
+    grahamScanFastAlgo(points, &S, n);
+
+    // print to file (with the correct format)
+    fp = fopen(outfilename, "w");
+    fprintf(fp, "%d\n", S.top+1);
+    for(j=0; j<S.top+1; j++)
+        fprintf(fp, "%9.6lf%11.6lf\n", S.points[j].x, S.points[j].y);
+
+
+    fclose(fp);
+
+    return 0;
+}
